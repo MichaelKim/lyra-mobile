aca49fc8bc1da529eba4b0e631c589f0a1e11746
Michael Kim <micklepickle98@gmail.com>
2021-04-02 11:59:07 -0400

    Fix white flash on stack navigate

--- src/components/library/index.tsx
+++ src/components/library/index.tsx
@@ -1,10 +1,12 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
+import { StyleSheet, View } from 'react-native';
 import { RootTabParamList } from '../../App';
+import { Colors } from '../../constants';
 import { PlaylistID, TabProps } from '../../types';
-import PlaylistDetail from './playlist/screen';
-import Library from './library';
 import ArtistScreen from './artist/screen';
+import Library from './library';
+import PlaylistDetail from './playlist/screen';
 
 const Stack = createStackNavigator<LibraryStackParamList>();
 
@@ -18,12 +20,26 @@ type Props = TabProps<RootTabParamList, 'Library'>;
 
 const Nav = (_: Props) => {
   return (
-    <Stack.Navigator initialRouteName="Library" headerMode="none">
-      <Stack.Screen name="Library" component={Library} />
-      <Stack.Screen name="Playlist" component={PlaylistDetail} />
-      <Stack.Screen name="Artist" component={ArtistScreen} />
-    </Stack.Navigator>
+    <View style={styles.view}>
+      <Stack.Navigator
+        initialRouteName="Library"
+        screenOptions={{
+          headerShown: false,
+          cardOverlayEnabled: false
+        }}>
+        <Stack.Screen name="Library" component={Library} />
+        <Stack.Screen name="Playlist" component={PlaylistDetail} />
+        <Stack.Screen name="Artist" component={ArtistScreen} />
+      </Stack.Navigator>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    backgroundColor: Colors.screen
+  }
+});
 
 export default Nav;
