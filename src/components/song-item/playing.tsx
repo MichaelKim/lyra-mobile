7a7fcd11218fdd191f7eebe93e0927710dbdd787
Michael Kim <micklepickle98@gmail.com>
2021-04-03 10:29:46 -0400

    Rewrite Playing with Reanimated 2

--- src/components/song-item/playing.tsx
+++ src/components/song-item/playing.tsx
@@ -1,80 +1,41 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import Animated, {
-  block,
-  Clock,
-  cond,
-  EasingNode,
-  interpolateNode,
-  set,
-  startClock,
-  stopClock,
-  timing,
-  Value
+  Easing,
+  interpolate,
+  useAnimatedStyle,
+  useSharedValue,
+  withRepeat,
+  withTiming
 } from 'react-native-reanimated';
 import { VolumeHigh, VolumeLow, VolumeMute } from '../../icons';
 
-// Change animation to static members?
-class Playing extends React.Component<{}> {
-  clock = new Clock();
-  clockState = {
-    finished: new Value(1),
-    position: new Value(0),
-    frameTime: new Value(0),
-    time: new Value(0)
-  };
-  config = {
-    toValue: 1,
-    duration: 2000,
-    easing: EasingNode.linear
-  };
+function Playing() {
+  const progress = useSharedValue(0);
+  progress.value = withRepeat(
+    withTiming(1, { duration: 2000, easing: Easing.linear }),
+    -1
+  );
 
-  position = block([
-    cond(
-      this.clockState.finished,
-      [
-        stopClock(this.clock),
-        set(this.clockState.finished, 0),
-        set(this.clockState.position, 0),
-        set(this.clockState.frameTime, 0),
-        set(this.clockState.time, 0),
-        startClock(this.clock)
-      ],
-      timing(this.clock, this.clockState, this.config)
-    ),
-    this.clockState.position
-  ]);
+  const lowStyle = useAnimatedStyle(() => ({
+    opacity: interpolate(progress.value, [0, 0.3, 0.7, 1], [0, 1, 1, 0])
+  }));
 
-  lowOpacity = interpolateNode(this.position, {
-    inputRange: [0, 0.3, 0.7, 1],
-    outputRange: [0, 1, 1, 0]
-  });
+  const highStyle = useAnimatedStyle(() => ({
+    opacity: interpolate(progress.value, [0, 0.3, 0.6, 0.7, 1], [0, 0, 1, 1, 0])
+  }));
 
-  highOpacity = interpolateNode(this.position, {
-    inputRange: [0, 0.3, 0.6, 0.7, 1],
-    outputRange: [0, 0, 1, 1, 0]
-  });
-
-  render() {
-    return (
-      <View style={[styles.root, styles.square]}>
-        <VolumeMute width={25} height={25} style={styles.mute} />
-        <Animated.View
-          style={[
-            styles.icon,
-            styles.square,
-            styles.low,
-            { opacity: this.lowOpacity }
-          ]}>
-          <VolumeLow width={25} height={25} />
-        </Animated.View>
-        <Animated.View
-          style={[styles.icon, styles.square, { opacity: this.highOpacity }]}>
-          <VolumeHigh width={25} height={25} />
-        </Animated.View>
-      </View>
-    );
-  }
+  return (
+    <View style={[styles.root, styles.square]}>
+      <VolumeMute width={25} height={25} style={styles.mute} />
+      <Animated.View style={[styles.icon, styles.square, styles.low, lowStyle]}>
+        <VolumeLow width={25} height={25} />
+      </Animated.View>
+      <Animated.View style={[styles.icon, styles.square, highStyle]}>
+        <VolumeHigh width={25} height={25} />
+      </Animated.View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
